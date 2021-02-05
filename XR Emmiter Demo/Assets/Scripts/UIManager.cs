cbeead363498a36952ad8580477c377de2cbebab
Oleg Frolov <frolololeg@gmail.com>
2021-02-05 23:07:20 +0300

    Added UIManger class

---
+++ XR Emmiter Demo/Assets/Scripts/UIManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI label;
+
+    private float _onTriggerIntensityValue;
+
+    private string _consoleLog;
+    private void Start()
+    {
+        XREmitterR.OnTriggerIntensity += SetOnTriggerIntensityValue;
+        
+    }
+
+    private void SetOnTriggerIntensityValue(float f)
+    {
+        _onTriggerIntensityValue = f;
+    }
+    
+    private void Update()
+    {
+        if (_consoleLog == null)
+        {
+            label.text = "no XREmitter";
+            return;
+        }
+
+        SetConsoleLogMessage();
+        
+        label.text = _consoleLog;
+    }
+
+    private void SetConsoleLogMessage()
+    {
+        _consoleLog = "OnTriggerIntensity: " + _onTriggerIntensityValue +
+                      " ";
+    }
+}
