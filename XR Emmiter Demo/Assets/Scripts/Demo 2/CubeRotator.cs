3c9ca46deff1649201777eced81218a4beb3c24a
Oleg Frolov <frolololeg@gmail.com>
2021-02-23 19:58:29 +0300

    Added CubeRotator

---
+++ XR Emmiter Demo/Assets/Scripts/Demo 2/CubeRotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotator : MonoBehaviour
+{
+    private const float RotSpeed = 30f;
+    
+    void Update()
+    {
+        transform.Rotate(Vector3.up, RotSpeed * Time.deltaTime);
+    }
+}
